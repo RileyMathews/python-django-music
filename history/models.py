6c811d0eb58126ebcebba78076a486ba51309028
Riley Mathews <rileymathews80@gmail.com>
2018-08-02 10:46:30 -0500

    added album table

--- history/models.py
+++ history/models.py
@@ -8,6 +8,15 @@ class Artist(models.Model):
     artist_name = models.CharField(max_length=200)
 
 class Song(models.Model):
-    Artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     song_name = models.CharField(max_length=200)
     song_length = models.IntegerField(default=200)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    album_name = models.CharField(max_length=200)
+    songs = models.ManyToManyField(Song, through='Album_Song')
+
+class Album_Song(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
